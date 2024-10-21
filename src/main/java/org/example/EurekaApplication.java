4711a5999be4064834783c6e85ac2fe063529e52
arlindhoxha <arlindhoxha007@gmail.com>
2024-10-21 14:18:06 -0400

    First commit

---
+++ src/main/java/org/example/EurekaApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Arlind Hoxha on 10/20/2024.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
